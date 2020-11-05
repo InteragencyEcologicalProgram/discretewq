c52c6b6790af0192c144edc401f9e6421adbaa95
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2020-11-05 12:58:12 -0800

    Updating test

--- tests/testthat/test-Data.R
+++ tests/testthat/test-Data.R
@@ -3,8 +3,7 @@ require(purrr)
 require(dplyr)
 
 All_rows<-sum(map_dbl(list(baystudy, DJFMP, EDSM, EMP, FMWT, SKT, STN, suisun, twentymm, USBR, USGS), nrow))
-Data<-wq(Sources = c("EMP", "STN", "FMWT", "EDSM", "DJFMP", "SKT",
-                     "20mm", "Suisun", "Baystudy", "USBR", "USGS"))%>%
+Data<-wq()%>%
   mutate(ID=paste(Source, Station, Date, Datetime))
 
 
@@ -14,4 +13,9 @@ test_that("All rows of data make it to the final dataset", {
 
 test_that("No sampes are duplicated", {
   expect_equal(length(unique(Data$ID)), nrow(Data))
+})
+
+test_that("All Lats are between 37 ad 39 and all Longs are between -123 and -121", {
+  expect_true(all((Data$Latitude<39 & Data$Latitude>37) | is.na(Data$Latitude)))
+  expect_true(all((Data$Longitude<(-121) & Data$Longitude>(-123)) | is.na(Data$Longitude)))
 })
