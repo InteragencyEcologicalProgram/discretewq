e1b29d67a8a6273a8f5360dd7adb9597daf418eb
seperry83 <seperry83@gmail.com>
2022-06-21 12:41:00 -0700

    update EMP data for 2021

--- data-raw/EMP.R
+++ data-raw/EMP.R
@@ -5,43 +5,41 @@ require(dplyr)
 require(tidyr)
 require(lubridate)
 
-# import data
-download.file("https://portal.edirepository.org/nis/dataviewer?packageid=edi.458.4&entityid=98b400de8472d2a3d2e403141533a2cc",
-              file.path(tempdir(), "SACSJ_delta_water_quality_1975_2020.csv"), mode="wb")
-download.file("https://portal.edirepository.org/nis/dataviewer?packageid=edi.458.4&entityid=827aa171ecae79731cc50ae0e590e5af",
-              file.path(tempdir(), "EMP_Discrete_Water_Quality_Stations_1975-2020.csv"), mode="wb")
+# import data from EDI to temp dir
+download.file("https://portal.edirepository.org/nis/dataviewer?packageid=edi.458.6&entityid=dfeaee030be901ae00b8c0449ea39e9c",
+              file.path(tempdir(), "SACSJ_delta_water_quality_1975_2021.csv"), mode="wb")
+download.file("https://portal.edirepository.org/nis/dataviewer?packageid=edi.458.6&entityid=ecf241d54a8335a49f8dfc8813d75609",
+              file.path(tempdir(), "EMP_Discrete_Water_Quality_Stations_1975-2021.csv"), mode="wb")
 
 # read in station data
-EMP_stations<-read_csv(file.path(tempdir(), "EMP_Discrete_Water_Quality_Stations_1975-2020.csv"),
-                       col_types=cols_only(Station="c", Latitude="d", Longitude="d"))%>%
+EMP_stations <-
+  read_csv(
+    file.path(tempdir(), "EMP_Discrete_Water_Quality_Stations_1975-2021.csv"),
+    col_types = cols_only(Station="c", Latitude="d", Longitude="d")
+    ) %>%
   drop_na()
 
-# read in RL data
-df_all_RLs <- read_csv(file.path('data-raw', 'EMP', 'emp_historic_RLs.csv'))
-df_TA_RLs <- read_csv(file.path('data-raw', 'EMP', 'emp_ta_RLs.csv')) %>%
-  pivot_wider(id_cols = c('Year','Month','Station'), names_from = Analyte, values_from = RL)
-
-# read in EMP data -- can't have ND be NA
-EMP <- read_csv(file.path(tempdir(), "SACSJ_delta_water_quality_1975_2020.csv"), na=c('NA'),
-              col_types = cols_only(Station="c", Date="c", Time="c", FieldNotes='c', Chla="c",
-                                    Depth="d", Secchi="d", Microcystis="d", SpCndSurface="d", SpCndBottom='d', DOSurface='d', DOBottom='d', DOpercentSurface='d', DOpercentBottom='d',
-                                    WTSurface="d", WTBottom='d', pHSurface = 'd', pHBottom = 'd', NorthLat='d', WestLong='d',
-                                    TotAlkalinity="c", TotAmmonia="c", DissAmmonia="c", DissBromide="c", DissCalcium="c", TotChloride="c", DissChloride="c", DissNitrateNitrite="c",
-                                    DOC="c", TOC="c", DON="c", TON="c", DissOrthophos="c", TotPhos="c",
-                                    DissSilica="c", TDS="c", TSS="c", VSS="c", TKN="c"))
-
-# remove NDs and convert to numeric for cols w/o RLs
-cols <- c('Chla','TotAlkalinity','TotAmmonia','DissBromide','DissCalcium','TotChloride','DissChloride','DOC','TOC','DON','TON','TotPhos','DissSilica','TDS','TSS','VSS','TKN')
-EMP[cols][EMP[cols] == 'ND'] <- NA
-EMP[cols] <- sapply(EMP[cols], as.numeric)
+# read in EMP data
+# data without sign (ie. all = to 2021): TotAlkalinity, TotAmmonia, TotChloride, DissChloride, TON, TDS
+EMP <- read_csv(file.path(tempdir(), "SACSJ_delta_water_quality_1975_2021.csv"),
+                col_types = cols_only(Station="c", Date="c", Time="c", FieldNotes='c', Chla_Sign="c", Chla="c", Depth="d", Secchi="d",
+                          Microcystis="d", SpCndSurface="d", SpCndBottom='d', DOSurface='d', DOBottom='d',
+                          DOpercentSurface='d', DOpercentBottom='d', WTSurface="d", WTBottom='d', pHSurface = 'd',
+                          pHBottom = 'd', NorthLat='d', WestLong='d', Pheophytin_Sign="c", Pheophytin="c", TotAlkalinity="c",
+                          TotAmmonia="c", DissAmmonia_Sign="c", DissAmmonia="c", DissBromide_Sign="c", DissBromide="c",
+                          DissCalcium_Sign="c", DissCalcium="c", TotChloride="c", DissChloride="c",
+                          DissNitrateNitrite_Sign="c", DissNitrateNitrite="c", DOC_Sign="c", DOC="c",
+                          TOC_Sign="c", TOC="c", DON_Sign="c",DON="c", TON="c", DissOrthophos_Sign="c",
+                          DissOrthophos="c", TotPhos_Sign="c", TotPhos="c", DissSilica_Sign="c", DissSilica="c",
+                          TDS="c", TSS_Sign="c", TSS="c", VSS_Sign="c", VSS="c", TKN_Sign="c", TKN="c"))
 
 # clean data w/o RLs
 EMP <- EMP %>%
-  rename(Notes=FieldNotes, Chlorophyll=Chla, Conductivity=SpCndSurface, Conductivity_bottom=SpCndBottom, Temperature=WTSurface, Temperature_bottom=WTBottom, DissolvedOxygen=DOSurface, DissolvedOxygen_bottom=DOBottom,
+  rename(Notes=FieldNotes, Chlorophyll=Chla, Chlorophyll_Sign=Chla_Sign, Conductivity=SpCndSurface, Conductivity_bottom=SpCndBottom, Temperature=WTSurface, Temperature_bottom=WTBottom, DissolvedOxygen=DOSurface, DissolvedOxygen_bottom=DOBottom,
          DissolvedOxygenPercent=DOpercentSurface, DissolvedOxygenPercent_bottom=DOpercentBottom, pH=pHSurface,
          pH_bottom = pHBottom, Latitude=NorthLat, Longitude=WestLong) %>%
-  mutate(Datetime=parse_date_time(if_else(is.na(Time), NA_character_, paste(Date, Time)), "%m/%d/%Y %H:%M", tz="Etc/GMT+8"), # EMP only reports time in PST, which corresponds to Etc/GMT+8 see https://stackoverflow.com/questions/53076575/time-zones-etc-gmt-why-it-is-other-way-round
-         Date=parse_date_time(Date, "%m/%d/%Y", tz="America/Los_Angeles"),
+  mutate(Datetime=parse_date_time(if_else(is.na(Time), NA_character_, paste(Date, Time)), "%Y-%m-%d %H:%M:%S", tz="Etc/GMT+8"), # EMP only reports time in PST, which corresponds to Etc/GMT+8 see https://stackoverflow.com/questions/53076575/time-zones-etc-gmt-why-it-is-other-way-round
+         Date=parse_date_time(Date, "%Y-%m-%d", tz="America/Los_Angeles"),
          Microcystis=round(Microcystis)) %>% #EMP has some 2.5 and 3.5 values
   select(-Time) %>%
   mutate(Datetime=if_else(hour(Datetime)==0, parse_date_time(NA_character_, tz="Etc/GMT+8"), Datetime) )%>%
@@ -57,32 +55,14 @@ EMP <- EMP %>%
     Latitude=if_else(is.na(Latitude), Latitude_field, Latitude),
     Longitude=if_else(is.na(Longitude), Longitude_field, Longitude))
 
-# -- RLs --
-# add sign cols
-EMP$DissAmmonia_Sign <- ifelse(is.na(EMP$DissAmmonia) | EMP$DissAmmonia != 'ND', '=', '<')
-EMP$DissNitrateNitrite_Sign <- ifelse(is.na(EMP$DissNitrateNitrite) | EMP$DissNitrateNitrite != 'ND', '=', '<')
-EMP$DissOrthophos_Sign <- ifelse(is.na(EMP$DissOrthophos) | EMP$DissOrthophos != 'ND', '=', '<')
-EMP$Year <- lubridate::year(EMP$Date)
-EMP$Month <- lubridate::month(EMP$Date)
-
-# merge RL cols
-EMP <- left_join(EMP, df_all_RLs, by = c('Year', 'Month'))
-EMP <- left_join(EMP, df_TA_RLs, by = c('Year', 'Month','Station'))
-EMP$DissAmmonia <- ifelse(EMP$DissAmmonia == 'ND', EMP$DissAmmonia_RL, EMP$DissAmmonia)
-EMP$DissNitrateNitrite <- ifelse(EMP$DissNitrateNitrite == 'ND', EMP$DissNitrateNitrite_RL, EMP$DissNitrateNitrite)
-EMP$DissNitrateNitrite <- ifelse(EMP$DissNitrateNitrite == 'ND', EMP$DissNitrateNitrite_TA_RL, EMP$DissNitrateNitrite)
-EMP$DissOrthophos <- ifelse(EMP$DissOrthophos == 'ND', EMP$DissOrthophos_RL, EMP$DissOrthophos)
-EMP$DissOrthophos <- ifelse(EMP$DissOrthophos == 'ND', EMP$DissOrthophos_TA_RL, EMP$DissOrthophos)
-
-# convert cols to numeric
-EMP$DissAmmonia <- as.numeric(EMP$DissAmmonia)
-EMP$DissOrthophos <- as.numeric(EMP$DissOrthophos)
-EMP$DissNitrateNitrite <- as.numeric(EMP$DissNitrateNitrite)
-
 # select cols
-EMP <- EMP %>% select(Source, Station, Latitude, Longitude, Field_coords, Date, Datetime, Notes, Depth, Tide, Microcystis, Chlorophyll, Secchi, Temperature, Temperature_bottom, Conductivity, Conductivity_bottom,
-       DissolvedOxygen, DissolvedOxygen_bottom, DissolvedOxygenPercent, DissolvedOxygenPercent_bottom, pH, pH_bottom, TotAlkalinity, TotAmmonia, DissAmmonia_Sign, DissAmmonia, DissBromide,
-       DissCalcium, TotChloride, DissChloride, DissNitrateNitrite_Sign, DissNitrateNitrite, DOC, TOC, DON, TON, DissOrthophos_Sign, DissOrthophos, TotPhos,
-       DissSilica, TDS, TSS, VSS, TKN)
+EMP <- EMP %>% select(Source, Station, Latitude, Longitude, Field_coords, Date, Datetime, Notes, Depth, Tide,
+                      Microcystis, Chlorophyll_Sign, Chlorophyll, Secchi, Temperature, Temperature_bottom,
+                      Conductivity, Conductivity_bottom, DissolvedOxygen, DissolvedOxygen_bottom,
+                      DissolvedOxygenPercent, DissolvedOxygenPercent_bottom, pH, pH_bottom, TotAlkalinity,
+                      TotAmmonia, DissAmmonia_Sign, DissAmmonia, DissBromide_Sign, DissBromide, DissCalcium_Sign,
+                      DissCalcium, TotChloride, DissChloride, DissNitrateNitrite_Sign, DissNitrateNitrite, DOC_Sign,
+                      DOC, TOC_Sign, TOC, DON_Sign, DON, TON, DissOrthophos_Sign, DissOrthophos, TotPhos_Sign,
+                      TotPhos, DissSilica_Sign, DissSilica, TDS, TSS_Sign, TSS, VSS_Sign, VSS, TKN_Sign, TKN)
 
 usethis::use_data(EMP, overwrite = TRUE)
