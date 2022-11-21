037e3e5285ed30cde2e739dd26060584f07813c8
Dave Bosworth <David.Bosworth@water.ca.gov>
2022-11-21 15:08:32 -0800

    Adding field coordinates to baystudy data set for the one station without coordinates

--- data-raw/baystudy.R
+++ data-raw/baystudy.R
@@ -5,6 +5,7 @@ require(dplyr)
 require(lubridate)
 require(tidyr)
 require(readxl)
+require(stringr)
 
 baystudy_stations <- read_excel(file.path("data-raw", "Baystudy", "Bay Study_Station Coordinates for Distribution_04May2020.xlsx"))%>%
   separate(Latitude, into=c("Lat_Deg", "Lat_Min"), sep = "°", convert=T)%>%
@@ -32,7 +33,8 @@ boatstation_baystudy <- read_csv(file.path("data-raw", "Baystudy", "BoatStation.
             by=c("Year", "Survey", "Station"))
 
 boattow_baystudy<-read_csv(file.path("data-raw", "Baystudy", "BoatTow.csv"),
-                           col_types = cols_only(Year="i", Survey="i", Station="c", Time="c", Tide="i"))%>%
+                           col_types = cols_only(Year="i", Survey="i", Station="c", Time="c", Tide="i",
+                                                 StartLat = "d", StartLong = "d"))%>%
   mutate(Time=parse_date_time(Time, orders="%m/%d/%Y %H:%M:%S"))%>%
   group_by(Year, Survey, Station)%>%
   filter(Time==min(Time))%>% # Just keep tide and time-of-day records at the time of the first tow
@@ -45,8 +47,46 @@ baystudy <- left_join(boattow_baystudy, boatstation_baystudy, by=c("Year", "Surv
   mutate(Tide=if_else(is.na(Tidetow), Tidestation, Tidetow), # If tide data were not collected at the time of the tow, use the value from the overall station visit
          Datetime=parse_date_time(if_else(is.na(Time), NA_character_, paste0(Date, " ", hour(Time), ":", minute(Time))), "%Y-%m-%d %H:%M", tz="America/Los_Angeles"),
          Source="Baystudy")%>%
-  left_join(baystudy_stations, by="Station")%>%
-  select(Source, Station, Latitude, Longitude, Date, Datetime, Depth, Tide, Secchi, Temperature=TempSurf, Temperature_bottom=TempBott, Conductivity=ECSurf)%>%
+  # Add field coordinates for the stations without lat-long coordinates
+  # From May 1981- April 1990, units were degrees, minutes and seconds; from May
+  # 1990 on, degrees, minutes and decimal minutes
+  mutate(
+    LatDeg = as.numeric(str_sub(StartLat, start = 1, end = 2)),
+    LatMin = if_else(
+      Date < "1990-05-01",
+      as.numeric(str_sub(StartLat, start = 3, end = 4)),
+      as.numeric(paste0(str_sub(StartLat, start = 3, end = 4), ".", str_sub(StartLat, start = 5)))
+    ),
+    LatSec = if_else(Date < "1990-05-01", as.numeric(str_sub(StartLat, start = 5)), NA_real_),
+    LongDeg = as.numeric(str_sub(StartLong, start = 1, end = 3)),
+    LongMin = if_else(
+      Date < "1990-05-01",
+      as.numeric(str_sub(StartLong, start = 4, end = 5)),
+      as.numeric(paste0(str_sub(StartLong, start = 4, end = 5), ".", str_sub(StartLong, start = 6)))
+    ),
+    LongSec = if_else(Date < "1990-05-01", as.numeric(str_sub(StartLong, start = 6)), NA_real_),
+    Latitude = if_else(
+      Date < "1990-05-01",
+      LatDeg + LatMin/60 + LatSec/3600,
+      LatDeg + LatMin/60
+    ),
+    Longitude = if_else(
+      Date < "1990-05-01",
+      (LongDeg + LongMin/60 + LongSec/3600) * -1,
+      (LongDeg + LongMin/60) * -1
+    )
+  ) %>%
+  left_join(baystudy_stations, by="Station", suffix = c("_field", ""))%>%
+  mutate(
+    Field_coords = case_when(
+      is.na(Latitude) & !is.na(Latitude_field) ~ TRUE,
+      is.na(Longitude) & !is.na(Longitude_field) ~ TRUE,
+      TRUE ~ FALSE
+    ),
+    Latitude = if_else(is.na(Latitude), Latitude_field, Latitude),
+    Longitude = if_else(is.na(Longitude), Longitude_field, Longitude)
+  ) %>%
+  select(Source, Station, Latitude, Longitude, Field_coords, Date, Datetime, Depth, Tide, Secchi, Temperature=TempSurf, Temperature_bottom=TempBott, Conductivity=ECSurf)%>%
   distinct(Source, Station, Date, Datetime, .keep_all=T)
 
 
