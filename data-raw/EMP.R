2421189adec852a1ce50f9f830c5422264c4b9f9
Sarah Perry <seperry83@gmail.comv>
2023-04-19 12:47:53 -0700

    fixed typo

--- data-raw/EMP.R
+++ data-raw/EMP.R
@@ -8,12 +8,12 @@ require(lubridate)
 download.file("https://portal.edirepository.org/nis/dataviewer?packageid=edi.458.8&entityid=cf231071093ac2861893793517db26f3",
               file.path(tempdir(), "EMP_DWQ_1975_2022.csv"), mode="wb")
 download.file("https://portal.edirepository.org/nis/dataviewer?packageid=edi.458.8&entityid=86dd696bc3f8407ff52954094e1e9dcf",
-              file.path(tempdir(), "EMP_DWQ_Stations_1975-202"), mode="wb")
+              file.path(tempdir(), "EMP_DWQ_Stations_1975-2022"), mode="wb")
 
 # read in station data
 EMP_stations <-
   read_csv(
-    file.path(tempdir(), "EMP_DWQ_Stations_1975-202"),
+    file.path(tempdir(), "EMP_DWQ_Stations_1975-2022"),
     col_types = cols_only(Station="c", Latitude="d", Longitude="d")
   ) %>%
   drop_na()
