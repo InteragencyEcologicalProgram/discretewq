5ad1e8204ceb36794fda77a61c5abe0cb5e5f79d
Dave Bosworth <David.Bosworth@water.ca.gov>
2024-03-11 08:51:26 -0700

    Minor formatting edits to DOP processing code

--- data-raw/DOP.R
+++ data-raw/DOP.R
@@ -1,41 +1,83 @@
-#Code to prepare DOP data goes here
-library(lubridate)
+# Code to prepare DOP data goes here
 library(tidyverse)
 
-#read in dataset
-DOPx = read_csv("https://portal.edirepository.org/nis/dataviewer?packageid=edi.1187.4&entityid=0dbed7163901c12df414da0762d28a86",
-                col_types=cols_only(Date="c", Latitude="d", Longitude="d",
-                                    Station_Code="c", Conductivity="d",
-                                    Temperature="d",  Secchi="d", Start_Time="c",
-                                      NO3 = "d", NH4 = "d",pH = "d",
-                               Chl_a = "d", PO4 = "d", Start_Depth = "d",
-                               Salinity = "d", Turbidity = "d", DOC = "d",
-                                    DO="d"))
+# read in dataset
+download.file(
+  "https://portal.edirepository.org/nis/dataviewer?packageid=edi.1187.4&entityid=0dbed7163901c12df414da0762d28a86",
+  file.path(tempdir(), "DOP_ICF_TowData_2017-2022.csv"),
+  mode = "wb",
+  method = "libcurl"
+)
 
-DOP= DOPx %>%
-  #standardize colom names
-  rename(Depth = Start_Depth,
-         Station = Station_Code,
-         Chlorophyll = Chl_a,
-         DissolvedOxygen = DO,
-         DissNitrateNitrite = NO3,
-         DissAmmonia = NH4,
-         DissOrthophos = PO4) %>%
-  #make a date-time column, convert feet to meters
-  mutate(        Date=parse_date_time(Date, c("%Y-%m-%d", "%m/%d/%Y"), tz="America/Los_Angeles"),
-                 Datetime = parse_date_time(if_else(is.na(Start_Time), NA_character_, paste(Date, Start_Time)), "%Y-%m-%d %H:%M:%S", tz="America/Los_Angeles"),
+DOPx <- read_csv(
+  file.path(tempdir(), "DOP_ICF_TowData_2017-2022.csv"),
+  col_types = cols_only(
+    Date = "c",
+    Latitude = "d",
+    Longitude = "d",
+    Station_Code = "c",
+    Habitat = "c",
+    Conductivity = "d",
+    Temperature = "d",
+    Secchi = "d",
+    Start_Time = "c",
+    NO3 = "d",
+    NH4 = "d",
+    pH = "d",
+    Chl_a = "d",
+    PO4 = "d",
+    Start_Depth = "d",
+    Salinity = "d",
+    Turbidity = "d",
+    DOC = "d",
+    DO = "d"
+  )
+)
 
-         Field_coords = TRUE, Source = "DOP", Depth = Depth*0.3048) %>%
-
-  #select relevent columns
-  #note - i'm not including microcystis because they use a different method
-  select(Source, Station, Latitude, Longitude,
-         Field_coords, Date, Datetime,
-          Depth,  Salinity,
-         Temperature, Turbidity, Conductivity,
-         pH, Chlorophyll, DissolvedOxygen, Secchi,
-        DOC,  DissNitrateNitrite, DissAmmonia, DissOrthophos)
-
+# Clean up data
+DOP <- DOPx %>%
+  # standardize column names
+  rename(
+    Depth = Start_Depth,
+    Station = Station_Code,
+    Chlorophyll = Chl_a,
+    DissolvedOxygen = DO,
+    DissNitrateNitrite = NO3,
+    DissAmmonia = NH4,
+    DissOrthophos = PO4
+  ) %>%
+  # make a date-time column, convert feet to meters
+  mutate(
+    Date = parse_date_time(Date, c("%Y-%m-%d", "%m/%d/%Y"), tz = "America/Los_Angeles"),
+    Datetime = parse_date_time(if_else(is.na(Start_Time), NA_character_, paste(Date, Start_Time)), "%Y-%m-%d %H:%M:%S", tz = "America/Los_Angeles"),
+    Field_coords = TRUE,
+    Source = "DOP",
+    Depth = Depth * 0.3048
+  ) %>%
+  # select relevant columns
+  # note - I'm not including Microcystis because they use a different method
+  select(
+    Source,
+    Station,
+    Latitude,
+    Longitude,
+    Field_coords,
+    Date,
+    Datetime,
+    Depth,
+    Salinity,
+    Temperature,
+    Turbidity,
+    Conductivity,
+    pH,
+    Chlorophyll,
+    DissolvedOxygen,
+    Secchi,
+    DOC,
+    DissNitrateNitrite,
+    DissAmmonia,
+    DissOrthophos
+  )
 
 usethis::use_data(DOP, overwrite = TRUE)
 
