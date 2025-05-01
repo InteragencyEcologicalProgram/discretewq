b6c25bd13c5e63521f2c74c7ac2bb17a99cb9a76
Dave Bosworth <David.Bosworth@water.ca.gov>
2025-05-01 15:32:17 -0700

    Updating DOP script with more automated workflow
    DOP data wasn't updated because its EDI publication hasn't changed since the last update

--- data-raw/DOP.R
+++ data-raw/DOP.R
@@ -1,75 +1,60 @@
-# Code to prepare DOP data goes here
-require(readr)
-require(dplyr)
-require(lubridate)
-require(conflicted)
+# Code to prepare `DOP` dataset
+library(readr)
+library(dplyr)
+library(lubridate)
+library(conflicted)
 
 # Declare package conflict preferences
 conflicts_prefer(dplyr::filter())
 
-# read in dataset
-download.file(
-  "https://portal.edirepository.org/nis/dataviewer?packageid=edi.1187.4&entityid=0dbed7163901c12df414da0762d28a86",
-  file.path(tempdir(), "DOP_ICF_TowData_2017-2022.csv"),
-  mode = "wb",
-  method = "libcurl"
-)
+source("data-raw/01_Global/data_raw_helpers.R")
 
-DOP_orig <- read_csv(
-  file.path(tempdir(), "DOP_ICF_TowData_2017-2022.csv"),
-  col_types = cols_only(
-    Date = "c",
-    Latitude = "d",
-    Longitude = "d",
-    Station_Code = "c",
-    Habitat = "c",
-    Conductivity = "d",
-    Temperature = "d",
-    Secchi = "d",
-    Start_Time = "c",
-    pH = "d",
-    Chl_a = "d",
-    Start_Depth = "d",
-    Salinity = "d",
-    Turbidity = "d",
-    DO = "d"
-  )
-)
+survey <- "DOP"
+
+# Check if there is a more recent EDI update
+edi_id_curr <- get_latest_edi_id(survey)
+
+# Compile and subset data entities for EDI data package
+edi_data_ent_all <- get_edi_data_entities(edi_id_curr)
+edi_data_ent_sub <- grep("^DOP_ICF_TowData", edi_data_ent_all, value = TRUE)
+
+# Download data entities to temporary directory
+get_edi_data(edi_id_curr, edi_data_ent_sub)
+
+# Determine file paths for data entities on temporary directory
+temp_files <- list.files(tempdir(), full.names = TRUE)
+file_data <- grep("DOP_ICF_TowData", temp_files, value = TRUE)
+
+# Import data and perform checks and minor processing
+DOP_orig <- import_raw_data(file_data, "DOP", "Data")
 
 # Clean up data
-# Note - We're not including Microcystis because they use a different method
+# Notes:
+  # We're not including Microcystis because they use a different method
+  # Turbidity is measured with a YSI EXO2 sonde according to the DOP methods - units are FNU
 DOP <- DOP_orig %>%
-  transmute(
-    Source = "DOP",
-    # Combine Station_Code and Habitat columns to make the Station column to
-    # preserve habitat info for each station
-    Station = paste(Station_Code, Habitat),
-    Habitat,
-    Latitude,
-    Longitude,
-    Field_coords = TRUE,
-    Date = ymd(Date, tz = "America/Los_Angeles"),
-    # Make a date-time column
-    Datetime = ymd_hms(if_else(is.na(Start_Time), NA_character_, paste(Date, Start_Time)), tz = "America/Los_Angeles"),
-    # Convert feet to meters
-    Depth = Start_Depth * 0.3048,
-    Secchi,
-    Temperature,
-    Salinity,
-    Conductivity,
-    DissolvedOxygen = DO,
-    pH,
-    # Turbidity is measured with a YSI EXO2 sonde according to the DOP methods - units are FNU
-    TurbidityFNU = Turbidity,
-    Chlorophyll = Chl_a
-  ) %>%
   # Remove Channel Deep and Oblique samples. Channel Deep measurements are taken
-  # at the bottom third to half of the water column and therefore aren't
-  # comparable to bottom samples from other surveys. The WQ measurements for the
-  # Oblique tows are either all NA or they are identical to either the Channel
-  # Surface or Channel Deep samples collected at the same location.
+    # at the bottom third to half of the water column and therefore aren't
+    # comparable to bottom samples from other surveys. The WQ measurements for the
+    # Oblique tows are either all NA or they are identical to either the Channel
+    # Surface or Channel Deep samples collected at the same location.
   filter(!Habitat %in% c("Channel Deep", "Oblique")) %>%
-  select(-Habitat) %>%
+  mutate(Source = "DOP", .before = 1) %>%
+  # Combine Station_Code and Habitat columns to make the Station column to
+    # preserve habitat info for each station
+  mutate(Station = paste(Station_Code, Habitat), .keep = "unused", .before = Latitude) %>%
+  mutate(Field_coords = TRUE, .after = Longitude) %>%
+  # Parse date and create a date-time column
+  mutate(
+    Date = ymd(Date, tz = "America/Los_Angeles"),
+    Datetime = ymd_hms(
+      if_else(is.na(Start_Time), NA_character_, paste(Date, Start_Time)),
+      tz = "America/Los_Angeles"
+    ),
+    .keep = "unused", .after = Date
+  ) %>%
+  # Convert feet to meters
+  mutate(Depth = Start_Depth * 0.3048, .keep = "unused", .before = Secchi) %>%
   # Remove replicate tows with identical WQ values - select earliest Datetime
   arrange(Datetime) %>%
   distinct(
@@ -89,3 +74,5 @@ DOP <- DOP_orig %>%
 
 usethis::use_data(DOP, overwrite = TRUE)
 
+document_helper_edi(edi_id_curr, DOP)
+update_edi_metadata(survey, edi_id_curr)
