b68425c2eca88015b5e01ae6029f84b2a3af2ea8
Dave Bosworth <David.Bosworth@water.ca.gov>
2025-09-17 10:55:29 -0700

    Updates to EMP processing code
    No changes to data - still using edi.458.9. Waiting for next EDI update to update data and switch to standardized workflow.

--- data-raw/EMP.R
+++ data-raw/EMP.R
@@ -1,92 +1,108 @@
 ## code to prepare `EMP` dataset goes here
-require(readr)
-require(dplyr)
-require(tidyr)
-require(lubridate)
+# >>> NOTE:
+# EMP is in the process of changing their data format from wide to long. However, this isn't
+# reflected in the current EDI data publication (458.12), and they are waiting for lab data results
+# from June-Dec 2024 before they update the data package in EDI. In addition, 458.12 doesn't
+# distinguish between the two Turbidity units (NTU and FNU). Therefore, we are waiting to update the
+# EMP dataset until the EDI data package is updated with the 1975-2024 data in the new long format.
+# For now, we're using the same EDI revision (458.9) that was used in the last discretewq update.
+# Once the EDI data package is updated with the 1975-2024 data in the long format, we'll update the
+# EMP dataset in discretewq using the standardized workflow for EDI data.
 
-# import data from EDI to temp dir
-download.file("https://portal.edirepository.org/nis/dataviewer?packageid=edi.458.9&entityid=cf231071093ac2861893793517db26f3",
-              file.path(tempdir(), "EMP_DWQ_1975_2022.csv"), mode="wb")
-download.file("https://portal.edirepository.org/nis/dataviewer?packageid=edi.458.9&entityid=86dd696bc3f8407ff52954094e1e9dcf",
-              file.path(tempdir(), "EMP_DWQ_Stations_1975-2022"), mode="wb")
+library(readr)
+library(dplyr)
+library(tidyr)
+library(stringr)
 
-# read in station data
+# Source helper functions
+source("data-raw/01_Global//data_raw_helpers.R")
+
+# Define settings for dataset
+survey <- "EMP"
+date_fmt <- "Ymd"
+time_fmt <- "HMS"
+tzone <- "Etc/GMT+8"
+secchi_unit <- "centimeters"
+depth_unit <- "feet"
+
+# Import data from EDI to temp dir
+# Compile all data entities for EDI data package 458.9
+edi_data_ent_all <- get_edi_data_entities("edi.458.9")
+
+# Subset to desired data entities
+edi_data_ent_sub <- str_subset(edi_data_ent_all, "2022$")
+
+# Download data entities to temporary directory
+get_edi_data("edi.458.9", edi_data_ent_sub)
+
+# List files in temporary directory
+temp_files <- list.files(tempdir(), full.names = TRUE)
+
+# Read in station data
 EMP_stations <-
   read_csv(
-    file.path(tempdir(), "EMP_DWQ_Stations_1975-2022"),
-    col_types = cols_only(Station="c", Latitude="d", Longitude="d")
+    str_subset(temp_files, "Stations"),
+    col_types = cols_only(Station = "c", Latitude = "d", Longitude = "d")
   ) %>%
   drop_na()
 
-# read in EMP data (two turbidity units, will change for 2023 to all FNU)
-EMP <- read_csv(file.path(tempdir(), "EMP_DWQ_1975_2022.csv"),
-                col_types = cols_only(
-                  Station="c", Date="c", Time="c", FieldNotes='c',
-                  Chla_Sign="c", Chla="d", Depth="d", Secchi="d", Microcystis="d",
-                  SpCndSurface="d", SpCndBottom='d', DOSurface='d', DOBottom='d',
-                  DOpercentSurface='d', DOpercentBottom='d', WTSurface="d", WTBottom='d',
-                  pHSurface = 'd', pHBottom = 'd',
-                  TurbiditySurface_NTU = 'd', TurbidityBottom_NTU = 'd', TurbiditySurface_FNU = 'd', TurbidityBottom_FNU = 'd',
-                  NorthLat='d', WestLong='d',
-                  Pheophytin_Sign="c", Pheophytin="d", TotAlkalinity_Sign="c", TotAlkalinity="d",
-                  TotAmmonia_Sign="c", TotAmmonia="d", DissAmmonia_Sign="c", DissAmmonia="d",
-                  DissBromide_Sign="c", DissBromide="d", DissCalcium_Sign="c", DissCalcium="d",
-                  TotChloride_Sign="c", TotChloride="d", DissChloride_Sign="c",DissChloride="d",
-                  DissNitrateNitrite_Sign="c", DissNitrateNitrite="d", DOC_Sign="c", DOC="d",
-                  TOC_Sign="c", TOC="d", DON_Sign="c", DON="d",
-                  TON_Sign="c", TON="d", DissOrthophos_Sign="c", DissOrthophos="d",
-                  TotPhos_Sign="c", TotPhos="d", DissSilica_Sign="c", DissSilica="d",
-                  TDS_Sign="c", TDS="d", TSS_Sign="c", TSS="d",
-                  VSS_Sign="c", VSS="d", TKN_Sign="c", TKN="d"
-                )
+# Read in EMP data (two turbidity units)
+EMP_raw <- read_csv(
+  str_subset(temp_files, "EMP_DWQ_1975_2022\\.bin$"),
+  col_types = cols_only(
+    Station = "c", Date = "c", Time = "c", FieldNotes = "c", Chla_Sign = "c", Chla = "d",
+    Depth = "d", Secchi = "d", Microcystis = "d", SpCndSurface = "d", SpCndBottom = "d",
+    DOSurface = "d", DOBottom = "d", DOpercentSurface = "d", DOpercentBottom = "d", WTSurface = "d",
+    WTBottom = "d", pHSurface = "d", pHBottom = "d", TurbiditySurface_NTU = "d",
+    TurbidityBottom_NTU = "d", TurbiditySurface_FNU = "d", TurbidityBottom_FNU = "d",
+    NorthLat = "d", WestLong = "d", Pheophytin_Sign = "c", Pheophytin = "d",
+    TotAlkalinity_Sign = "c", TotAlkalinity = "d", TotAmmonia_Sign = "c", TotAmmonia = "d",
+    DissAmmonia_Sign = "c", DissAmmonia = "d", DissBromide_Sign = "c", DissBromide = "d",
+    DissCalcium_Sign = "c", DissCalcium = "d", TotChloride_Sign = "c", TotChloride = "d",
+    DissChloride_Sign = "c", DissChloride = "d", DissNitrateNitrite_Sign = "c",
+    DissNitrateNitrite = "d", DOC_Sign = "c", DOC = "d", TOC_Sign = "c", TOC = "d", DON_Sign = "c",
+    DON = "d", TON_Sign = "c", TON = "d", DissOrthophos_Sign = "c", DissOrthophos = "d",
+    TotPhos_Sign = "c", TotPhos = "d", DissSilica_Sign = "c", DissSilica = "d", TDS_Sign = "c",
+    TDS = "d", TSS_Sign = "c", TSS = "d", VSS_Sign = "c", VSS = "d", TKN_Sign = "c", TKN = "d"
+  )
 )
 
 # clean data
-EMP <- EMP %>%
+EMP <- EMP_raw %>%
   rename(
-    Notes=FieldNotes, Chlorophyll=Chla, Chlorophyll_Sign=Chla_Sign,
-    Conductivity=SpCndSurface, Conductivity_bottom=SpCndBottom,
-    Temperature=WTSurface, Temperature_bottom=WTBottom,
-    DissolvedOxygen=DOSurface, DissolvedOxygen_bottom=DOBottom,
-    DissolvedOxygenPercent=DOpercentSurface, DissolvedOxygenPercent_bottom=DOpercentBottom,
+    Notes = FieldNotes, Chlorophyll = Chla, Chlorophyll_Sign = Chla_Sign,
+    Conductivity = SpCndSurface, Conductivity_bottom = SpCndBottom, Temperature = WTSurface,
+    Temperature_bottom = WTBottom, DissolvedOxygen = DOSurface, DissolvedOxygen_bottom = DOBottom,
+    DissolvedOxygenPercent = DOpercentSurface, DissolvedOxygenPercent_bottom = DOpercentBottom,
     TurbidityFNU = TurbiditySurface_FNU, TurbidityFNU_bottom = TurbidityBottom_FNU,
-    TurbidityNTU = TurbiditySurface_NTU, TurbidityNTU_bottom = TurbidityBottom_NTU,
-    pH=pHSurface, pH_bottom = pHBottom,
-    Latitude=NorthLat, Longitude=WestLong
-    ) %>%
-  mutate(Datetime=parse_date_time(if_else(is.na(Time), NA_character_, paste(Date, Time)), "%Y-%m-%d %H:%M:%S", tz="Etc/GMT+8"), # EMP only reports time in PST, which corresponds to Etc/GMT+8 see https://stackoverflow.com/questions/53076575/time-zones-etc-gmt-why-it-is-other-way-round
-         Date=parse_date_time(Date, "%Y-%m-%d", tz="America/Los_Angeles"),
-         Microcystis=round(Microcystis)) %>% #EMP has some 2.5 and 3.5 values
-  select(-Time) %>%
-  mutate(Datetime=if_else(hour(Datetime)==0, parse_date_time(NA_character_, tz="Etc/GMT+8"), Datetime) )%>%
-  mutate(Datetime=with_tz(Datetime, tz="America/Los_Angeles")) %>% # Since EMP records only in PST, convert to local time to correspond with the other surveys
-  mutate(Source="EMP",
-         Tide = "High Slack",
-         Depth=Depth*0.3048) %>% # Convert feet to meters
-  left_join(EMP_stations, by="Station", suffix=c("_field", '')) %>%
-  mutate(Field_coords=case_when(
-    is.na(Latitude) & !is.na(Latitude_field) ~ TRUE,
-    is.na(Longitude) & !is.na(Longitude_field) ~ TRUE,
-    TRUE ~ FALSE),
-    Latitude=if_else(is.na(Latitude), Latitude_field, Latitude),
-    Longitude=if_else(is.na(Longitude), Longitude_field, Longitude))
-
-# select cols
-EMP <- EMP %>% select(
-  Source, Station, Latitude, Longitude, Field_coords, Date, Datetime, Notes, Depth, Tide,
-  Microcystis, Chlorophyll_Sign, Chlorophyll, Secchi,
-  Temperature, Temperature_bottom, Conductivity, Conductivity_bottom,
-  DissolvedOxygen, DissolvedOxygen_bottom, DissolvedOxygenPercent, DissolvedOxygenPercent_bottom,
-  pH, pH_bottom, TurbidityNTU, TurbidityNTU_bottom, TurbidityFNU, TurbidityFNU_bottom,
-  Pheophytin_Sign, Pheophytin,
-  TotAlkalinity_Sign, TotAlkalinity, TotAmmonia_Sign, TotAmmonia,
-  DissAmmonia_Sign, DissAmmonia, DissBromide_Sign, DissBromide,
-  DissCalcium_Sign, DissCalcium, TotChloride_Sign, TotChloride,
-  DissChloride_Sign, DissChloride, DissNitrateNitrite_Sign, DissNitrateNitrite,
-  DOC_Sign, DOC, TOC_Sign, TOC,
-  DON_Sign, DON, TON_Sign, TON,
-  DissOrthophos_Sign, DissOrthophos, TotPhos_Sign, TotPhos,
-  DissSilica_Sign, DissSilica, TDS_Sign, TDS,
-  TSS_Sign, TSS, VSS_Sign, VSS, TKN_Sign, TKN)
+    TurbidityNTU = TurbiditySurface_NTU, TurbidityNTU_bottom = TurbidityBottom_NTU, pH = pHSurface,
+    pH_bottom = pHBottom, Latitude = NorthLat, Longitude = WestLong
+  ) %>%
+  convert_datetime(date_fmt, time_fmt, tzone) %>%
+  mutate(
+    # EMP has some 1.5, 2.5 and 3.5 values
+    Microcystis = round(Microcystis),
+    # EMP always collects samples at High Slack
+    Tide = "High Slack"
+  ) %>%
+  # Convert feet to meters
+  convert_depth(depth_unit) %>%
+  # Add coordinates
+  left_join(EMP_stations, by = join_by(Station), suffix = c("_field", "")) %>%
+  mutate(
+    Field_coords = case_when(
+      is.na(Latitude) & !is.na(Latitude_field) ~ TRUE,
+      is.na(Longitude) & !is.na(Longitude_field) ~ TRUE,
+      .default = FALSE
+    ),
+    Latitude = if_else(is.na(Latitude), Latitude_field, Latitude),
+    Longitude = if_else(is.na(Longitude), Longitude_field, Longitude),
+    .keep = "unused"
+  ) %>%
+  # Remove rows where all measurements are NA, if they exist
+  rm_rows_all_miss_data() %>%
+  # Add source variable
+  add_source_col(survey) %>%
+  standardize_col_order()
 
 usethis::use_data(EMP, overwrite = TRUE)
